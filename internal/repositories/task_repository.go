1db776f5613e741dc7279ab48aa4bc4c96742e1d
bishalkl <bishal123koirala@gmail.com>
2025-05-11 19:43:06 +0545

    done

--- internal/repositories/task_repository.go
+++ internal/repositories/task_repository.go
@@ -1,1 +1,21 @@
 package repositories
+
+import "gorm.io/gorm"
+
+type TaskRepository interface {
+}
+
+type TaskRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func NewTaskRepository(db *gorm.DB) TaskRepository {
+	return &TaskRepositoryImpl{
+		DB: db,
+	}
+}
+
+// create
+// update
+// retrieve
+// delete
